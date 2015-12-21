2599fdd5cf2266adaace6ff3dc35ad983c134404
Metuz <dead.man99@hotmail.com>
2015-12-21 16:51:31 -0600

    Adding test

--- test/models/academy_test.rb
+++ test/models/academy_test.rb
@@ -4,4 +4,27 @@ class AcademyTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @academy = Academy.new(name: "Programacion I")
+  end
+
+  test "should be valid" do
+    assert @academy.valid?
+  end
+
+  test "name should be present" do
+    @academy.name = ""
+    assert_not @academy.valid?
+  end
+
+  test "name shouldn't not too long" do
+    @academy.name = "a" * 41
+    assert_not @academy.valid?
+  end
+
+  test "academy's name shouldn't repeat" do
+    @academy_dup = @academy.dup
+    @academy_dup.save
+    assert_not @academy_dup.valid?
+  end
 end
