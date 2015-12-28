ad9271b5a96b3b15c0e050ae77cb055793208c45
Metuz <dead.man99@hotmail.com>
2015-12-28 15:46:05 -0600

    Creating Master Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :masters
+
   resources :subjects
 
   resources :teachers
---
+++ db/migrate/20151228214541_create_masters.rb
@@ -0,0 +1,9 @@
+class CreateMasters < ActiveRecord::Migration
+  def change
+    create_table :masters do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
