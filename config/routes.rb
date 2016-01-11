fd38d8b7c1f74bc0c2a3dbc85da891e11b5999b1
Metuz <dead.man99@hotmail.com>
2016-01-11 14:09:22 -0600

    Adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+
   devise_for :users
   resources :majors
 
@@ -15,6 +17,7 @@ Rails.application.routes.draw do
   end
 
   resources :academies
+  root 'welcome#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
